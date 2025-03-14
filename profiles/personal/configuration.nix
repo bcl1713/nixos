1eb7df8ee4e90e6e5b40b990990baf98de88413a
Brian Lucas <bcl1713@gmail.com>
2025-03-13 20:21:06 -0500

    feat: hyprland enable

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -46,8 +46,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
 	programs.hyprland = {
 	  enable = true;
@@ -103,6 +103,7 @@
     wget
     git
     zsh
+		wofi
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
